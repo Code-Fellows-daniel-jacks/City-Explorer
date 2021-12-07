c4974d86fb226b9397986864159483c85514beb9
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-12-06 17:33:30 -0800

    creates Location component and tucks away the rendered location card inside

---
+++ src/components/Location.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+
+export default class Location extends Component {
+    render() {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>{this.props.renderLocation.display_name}</Card.Title>
+                    <Card.Img />
+                    <Card.Text>
+                        {(this.props.renderLocation.lat)}, {(this.props.renderLocation.lon)}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,10 @@
 import React, { Component } from "react";
-import Card from 'react-bootstrap/Card';
+import Location from './Location.js';
 
 export default class Main extends Component {
     render() {
         return (
-            <Card>
-                <Card.Body>
-                    <Card.Title>{this.props.renderLocation.display_name}</Card.Title>
-                    <Card.Img />
-                    <Card.Text>
-                        {this.props.renderLocation.lat}, {this.props.renderLocation.lon}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <Location renderLocation={this.props.renderLocation} />
         )
     }
 }
